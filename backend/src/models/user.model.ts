e667bbb625cba9c192f3fb3f06d3c143b4d70596
vanshpitale <vanssh24@gmail.com>
2026-06-01 23:57:36 +0530

    feat: implemented the user model

---
+++ backend/src/models/user.model.ts
@@ -0,0 +1,57 @@
+import mongoose from "mongoose";
+import { compareValue, hashValue } from "../utils/bcrypt";
+
+export interface UserDocument extends mongoose.Document {
+    name: string;
+    email: string;
+    password: string;
+    profilePicture: string | null;
+    createdAt: Date;
+    updatedAt: Date;
+    comparePassword: (password: string) => Promise<boolean>;
+    omitPassword: () => Omit<UserDocument, "password">;
+}
+
+const userSchema = new mongoose.Schema<UserDocument>({
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+    },
+    password: {
+        type: String,
+        select: true,
+        required: true,
+    },
+
+}, { timestamps: true });
+
+userSchema.pre("save", async function (next) {
+    if (this.isModified("password")) {
+        if (this.password) {
+            this.password = await hashValue(this.password);
+        }
+    }
+});
+
+userSchema.methods.omitPassword = function (): Omit<UserDocument, "password"> {
+    const userObject = this.toObject();
+    delete userObject.password;
+    return userObject;
+};
+
+
+userSchema.methods.comparePassword = async function (password: string): Promise<boolean> {
+    return compareValue(password, this.password);
+}
+
+const UserModel = mongoose.model<UserDocument>("User", userSchema);
+
+export default UserModel;
