294973bd6ce254ab40153b7a29f5493f21fd7901
vanshpitale <vanssh24@gmail.com>
2026-06-02 07:30:47 +0530

    feat: implemented the report model

---
+++ backend/src/models/report.model.ts
@@ -0,0 +1,43 @@
+import mongoose from "mongoose";
+
+export enum ReportStatusEnum {
+    SENT = "SENT",
+    PENDING = "PENDING",
+    FAILED = "FAILED",
+};
+
+export interface ReportDocument extends mongoose.Document {
+    userId: mongoose.Types.ObjectId;
+    period: string;
+    sentDate: Date;
+    status: keyof typeof ReportStatusEnum;
+    createdAt: Date;
+    updatedAt: Date;
+}
+
+const reportSchema = new mongoose.Schema<ReportDocument>({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: "User"
+    },
+    period: {
+        type: String,
+        required: true
+    },
+    sentDate: {
+        type: Date,
+        required: true,
+    },
+    status: {
+        type: String,
+        enum: Object.values(ReportStatusEnum),
+        default: ReportStatusEnum.PENDING
+    }
+}, {
+    timestamps: true
+});
+
+const ReportModel = mongoose.model<ReportDocument>("Report", reportSchema);
+
+export default ReportModel;
